48aa0dde078c1bc9c5dde529f676601499c9d3e1
M <thanaporn_chan@cmu.ac.th>
2021-12-22 15:18:57 +0700

    13_3 complete

--- lab13_3.cpp
+++ lab13_3.cpp
@@ -2,42 +2,23 @@
 
 using namespace std;
 
-template <typename currentType>
-void mySwap(currentType &, currentType &);
+void mySwap(int &,int &); 
 
-int main()
-{
-	int x, y;
-	string a, b;
-	char p, q;
-	
-	cin >> x >> y >> a >> b >> p >> q;
-	
+int main(){
+	int x,y;
+	cin >> x >> y;
 	cout << "Before swapping:\n";
 	cout << "x = " << x << ", y = " << y << "\n";
-	mySwap(x,y);
+	mySwap (x,y);
 	cout << "After swapping:\n";
 	cout << "x = " << x << ", y = " << y << "\n";
 	
-	cout << "Before swapping:\n";
-	cout << "a = " << a << ", b = " << b << "\n";
-	mySwap(a,b);
-	cout << "After swapping:\n";
-	cout << "a = " << a << ", b = " << b << "\n";
-	
-	cout << "Before swapping:\n";
-	cout << "p = " << p << ", q = " << q << "\n";
-	mySwap(p,q);
-	cout << "After swapping:\n";
-	cout << "p = " << p << ", q = " << q << "\n";
-
 	return 0;
 }
 
-template <typename currentType>
-void mySwap(currentType &x,currentType &y)
+void mySwap(int &x, int &y)
 {
-	currentType temp = x;
+	int temp = x;
 	x = y;
 	y = temp;
 }
